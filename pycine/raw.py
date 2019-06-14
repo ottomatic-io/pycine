0891500907fe4f3c9f3df2ae0376958c4d250749
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2019-06-14 23:22:26 +0900

    bugfix for image_generator function

--- pycine/raw.py
+++ pycine/raw.py
@@ -84,13 +84,14 @@ def image_generator(cine_file, start_frame=False, start_frame_cine=False, count=
     raw_image_generator : generator
         A generator for raw image
     """
+    header = read_header(cine_file)
     if type(start_frame) == int and type(start_frame_cine) == int:
         raise ValueError("Do not specify both of start_frame and start_frame_cine")
-    header = read_header(cine_file)
-
-    if type(start_frame) == int:
+    elif start_frame == False and start_frame_cine == False:
+        fetch_head = 1
+    elif type(start_frame) == int:
         fetch_head = start_frame
-    if type(start_frame_cine) == int:
+    elif type(start_frame_cine) == int:
         numfirst = header["cinefileheader"].FirstImageNo
         numlast = numfirst + header["cinefileheader"].ImageCount-1
         fetch_head = start_frame_cine - numfirst
